f8ba2988bc3a7fc22d4b82acbff3a288133b35eb
Home <prakashsingh20497@gmail.com>
2024-03-05 16:21:05 +0530

    added

--- sheet/src/components/Contactas.jsx
+++ sheet/src/components/Contactas.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import { TextField, Button, Typography, Container, Grid } from '@mui/material';
-import axios from 'axios';
 
 export default function Contactas() {
     const [formData, setFormData] = useState({
@@ -18,95 +17,87 @@ export default function Contactas() {
   
     const handleSubmit = (e) => {
       e.preventDefault();
-      axios.post(
-        "https://script.google.com/macros/s/AKfycbz8q0WDG4WlwyH09BcZy7bHsgghZGIlPsz9NEbubs3uDjvSMLYpWOO27bZ0dB2AW0jp/exec",
-        formData
+      fetch(
+       "https://script.google.com/macros/s/AKfycbzPncumcoIf5MEhRfVXUnf-S7OXI-_FQxp1N9MpwCliNGqKPPFV3n8jE7xzmpb-AXeydA/exec",
+        {
+          method: "POST",
+          body: new FormData(e.target)
+        }
       )
-        .then((response) => {
-          console.log(response.data);
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
           setSubmissionStatus('success');
         })
         .catch((error) => {
-          console.error('Error submitting data:', error);
+          console.log(error);
           setSubmissionStatus('error');
         });
     };
   
     return (
-      
       <Container 
-        maxWidth="md" 
+        maxWidth="md"
         style={{ 
-          display: 'flex', 
-          flexDirection: 'column', 
-          justifyContent: 'center', 
-          alignItems: 'center', 
-          marginTop: '50px', 
-          marginBottom: '50px',
-          color:"#646cffaa",
-          height: '100vh', 
+          height: '100vh', // Full viewport height
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center'
         }}
       >
-        <Typography variant="h4">Contact Me form</Typography>
-        <div  style={{ width: '100%', h:"100%", border:"1px solid red",display: 'flex',  
-          alignItems: 'center', }}>
-             <form onSubmit={handleSubmit} style={{ width: '100%', h:"100%" }}>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <TextField
-                label="Your Name"
-                name="Name"
-                value={formData.Name}
-                onChange={handleChange}
-                fullWidth
-              />
+        <div>
+          <Typography variant="h4">Contact Me form</Typography>
+          <form onSubmit={handleSubmit}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  label="Your Name"
+                  name="Name"
+                  value={formData.Name}
+                  onChange={handleChange}
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  label="Your Email"
+                  name="Email"
+                  value={formData.Email}
+                  onChange={handleChange}
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  label="Your Phone No"
+                  name="PhoneNo"
+                  value={formData.PhoneNo}
+                  onChange={handleChange}
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  label="Your Message"
+                  name="Message"
+                  value={formData.Message}
+                  onChange={handleChange}
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Button variant="contained" color="primary" type="submit">
+                  Submit
+                </Button>
+              </Grid>
             </Grid>
-            <Grid item xs={12}>
-              <TextField
-                label="Your Email"
-                name="Email"
-                value={formData.Email}
-                onChange={handleChange}
-                fullWidth
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                label="Your Phone No"
-                name="PhoneNo"
-                value={formData.PhoneNo}
-                onChange={handleChange}
-                fullWidth
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                label="Your Message"
-                name="Message"
-                value={formData.Message}
-                onChange={handleChange}
-                fullWidth
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <Button variant="contained" color="primary" type="submit">
-                Submit
-              </Button>
-            </Grid>
-          </Grid>
-        </form>
-        {submissionStatus === 'success' && (
-          <Typography variant="body1" style={{ color: 'green' }}>
-            Data submitted successfully!
-          </Typography>
-        )}
-        {submissionStatus === 'error' && (
-          <Typography variant="body1" style={{ color: 'red' }}>
-            Error submitting data. Please try again later.
-          </Typography>
-        )}
+          </form>
+          {submissionStatus === 'success' && (
+            <Typography variant="body1" style={{ color: 'green' }}>
+              Data submitted successfully!
+            </Typography>
+          )}
         </div>
-     
       </Container>
     );
 }
